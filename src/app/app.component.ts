104c0e5f4b7f2e1d027b87dafef4026b39996aba
nashran95 <58025609+nashran95@users.noreply.github.com>
2020-09-14 01:15:55 +0800

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  //sorting
+  key: string = 'name'; //set default
+  reverse: boolean = false;
+  sort(key){
+    this.key = key;
+    this.reverse = !this.reverse;
+  }
+
+  //initializing p to one
+  p: number = 1;
+}
+
---
+++ src/app/models/searchHistory.ts
@@ -0,0 +1,4 @@
+export class InputHistory
+{
+  title: string;
+}
